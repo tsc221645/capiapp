e0ebc9e14a6e23d02b1a727815b83793c775b2ab
tsc221645 <tsc221645@uvg.edu.gt>
2025-03-28 18:46:48 -0600

    fix: frontend

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,3 @@
 module github.com/tuusuario/capiapp
 
-go 1.24.1
+go 1.21
--- capiapp/vue.config.js
+++ capiapp/vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// capiapp/vue.config.js
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 5173
+  }
+}

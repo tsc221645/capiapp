520ac146a3dbdb1dd7895b3a2d4ab0dc2c652863
tsc221645 <tsc221645@uvg.edu.gt>
2025-05-15 15:59:01 -0600

    fix: config vue

--- capiapp/vue.config.js
+++ capiapp/vue.config.js
@@ -1,13 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-// capiapp/vue.config.js
-module.exports = {
+  transpileDependencies: true,
   publicPath: '/',
   devServer: {
     host: '0.0.0.0',
     port: 5173
-    
   }
-}
+})
